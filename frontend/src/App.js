6929993873b1d9ab3bdfb756fc62d342fb27de5c
Rahul <rn.007.shah@gmail.com>
2024-06-19 12:23:23 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+import './App.css';
+import { Button, Col, Container, Row } from "react-bootstrap";
+
+
+
+function App() {
+  
+  
+
+  return (
+    <Container>
+      App
+    </Container>
+  )
+}
+
+export default App;
